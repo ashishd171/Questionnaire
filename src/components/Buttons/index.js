03dce3eccc31ede8fcc10d60ae125efe65397729
ashish <ashishd@keymouseit.com>
2022-02-04 18:11:06 +0530

    changes

--- src/components/Buttons/index.js
+++ src/components/Buttons/index.js
@@ -6,12 +6,9 @@ import { Wrapper } from './style';
 const Buttons = ({submitClick , previousClick, submit}) => {
     return (
         <Wrapper>
-            <div className="container btn-sec" style={{display: 'inline-block'}}>
+            <div className="container btn-sec">
                 <Button onClick={previousClick} className="prevbtn text-uppercase  text-darker"><AiOutlineArrowLeft  className="arrow-left" />previous</Button> 
                 <Button onClick={submitClick} className="submitbtn text-uppercase  text-darker">{submit}<AiOutlineArrowRight className="arrow-right"  /></Button> 
-                {/* for testing  */}
-                <Button className="submitbtn text-uppercase  text-darker">submit2<AiOutlineArrowRight className="arrow-right"  /></Button>
-                <Button className="submitbtn text-uppercase  text-darker">submit3<AiOutlineArrowRight className="arrow-right"  /></Button>   
             </div>
         </Wrapper>
     );
